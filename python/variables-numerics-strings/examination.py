c8c62ddae8f9c8f4845d4df19a786a24092f7a43
malinborn <rikkeisy@gmail.com>
2024-10-27 21:08:49 +0300

    fixed third task on strings

--- python/variables-numerics-strings/examination.py
+++ python/variables-numerics-strings/examination.py
@@ -5,8 +5,8 @@ def test_average(x, y, z, average):
 def test_info(name, age, info):  
     assert info == f"Меня зовут {name}. Мне {age} лет.", "Ошибка в формате строки info"  
   
-def test_string_methods(sentence, uppercase_sentence):  
-    assert uppercase_sentence == sentence.capitalize(), f"Должно получиться \"{sentence.capitalize()}\", а у тебя \"{uppercase_sentence}\""  
+def test_string_methods(sentence, student_sentence):  
+    assert student_sentence == sentence.title(), f"Должно получиться \"{sentence.title()}\", а у тебя \"{student_sentence}\""  
   
 def test_number_conversion(number_str, number):  
     assert number == int(number_str), "Ошибка в преобразовании строки в число"  
