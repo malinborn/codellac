2a89bc4cb5da701886bbb31485497890e8721983
malinborn <rikkeisy@gmail.com>
2024-11-24 19:41:05 +0300

    added answer

--- python/9-class/bestiary/knight.py
+++ python/9-class/bestiary/knight.py
@@ -6,27 +6,7 @@ from world.health import Health
 
 
 class Knight:
-    NOBLE_SWORD_NAME = "Экскалибур"
-
-    def __init__(self, name):
-        self.name = name
-        self.equipment = Equipment()
-        self.health = Health.give_birth_a_knight()
-
-    def check_shield(self):
-        return bool(self.equipment.shield.health)
-
-    def loads_crossbow(self, bolts: int):
-        for bolt in range(bolts):
-            self.equipment.crossbow.load()
-
-    @staticmethod
-    def pronoun_an_elder(elder: str) -> str:
-        return f"Мой славный предок, рыцарь {elder}"
-
-    def __repr__(self):
-        return f"Вот я — {self.name}, мой меч — {self.equipment.sword.name}!"
-
+    pass
 
 @dataclass
 class Equipment:
